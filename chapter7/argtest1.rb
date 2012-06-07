6a8cf8545f61293b0b2155923dc09e6b4118c24f
kimihito <tatsurotamashiro@gmail.com>
2012-06-08 08:38:59 +0900

    chapter7-1

---
+++ chapter7/argtest1.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+#-*- coding: utf-8 -*-
+puts "引数１：#{ARGV[0]}"
+puts "引数２：#{ARGV[1]}"
+puts "引数３：#{ARGV[2]}"
+
+p ARGV
+
---
+++ chapter7/stdout.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#-*- coding: utf-8 -*-
+$stdout.puts "標準出力へのメッセージ"
+$stderr.puts "標準エラー出力へのメッセージ"
